799cec863a88b98e630a3541dc1020b14984731a
korzhevdp <korzhevdp@gmail.com>
2017-07-13 18:06:30 +0300

    refactoring

---
+++ application/models/Userdatamodel.php
@@ -0,0 +1,221 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Userdatamodel extends CI_Model {
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	/*  URL Retrieve  */
+
+	/**
+	* Returns an URL
+	* @param string $URLType: code|token|user|address|contacts
+	* @return string
+	*/
+
+	public function getURL($URLType='name') {
+		$URLS = array(
+			'code'       => 'aas/oauth2/ac?',
+			'token'      => 'aas/oauth2/te',
+			'name'       => 'rs/prns/'.$this->oid,
+			'birthplace' => 'rs/prns/'.$this->oid,
+			'address'    => 'rs/prns/'.$this->oid.'/addrs',
+			'contacts'   => 'rs/prns/'.$this->oid.'/ctts'
+		);
+		return $this->portalUrl.$URLS[$URLType];
+	}
+
+	public $fullname       = null;
+	public $reg_region     = null;
+	public $reg_city       = null;
+	public $reg_street     = null;
+	public $reg_house      = null;
+	public $reg_frame      = null;
+	public $reg_flat       = null;
+	public $reg_fias       = null;
+	public $plv_region     = null;
+	public $plv_city       = null;
+	public $plv_street     = null;
+	public $plv_house      = null;
+	public $plv_frame      = null;
+	public $plv_flat       = null;
+	public $plv_fias       = null;
+	public $birthplace     = null;
+	public $email          = null;
+	public $cel_ph         = null;
+	public $trusted        = null;
+
+	private $reqmode = null;
+
+	/* DATA GETTERS */
+
+	/**
+	* Returns User Data object contents
+	* 
+	* @param $token string
+	* @return string|false
+	*/
+	public function requestUserData($token="", $mode = 'name') {
+		if ( !strlen($token) ) {
+			$this->logmodel->addToLog("Access token is missing. Aborting\n");
+			return false;
+		}
+		if ( !strlen($this->oid) ) {
+			$this->logmodel->addToLog("Object ID is missing. Aborting\n");
+			return false;
+		}
+		$this->logmodel->addToLog("\n------------------#-#-#------------------\nRequesting User Data\n");
+
+		$url = $this->getURL($mode);
+		$result  = json_decode(file_get_contents($url, false, $this->getRequestContext($token)));
+		//print nl2br(str_replace(" ", "&nbsp;", print_r($result, true)));
+
+		$this->logmodel->addToLog("\nUSER DATA REQUEST SUCCESS\n".print_r($result, true));
+		
+		if ($mode === 'birthplace') {
+			$this->birthplace = $result->birthPlace;
+		}
+
+		if ($mode === 'name') {
+			$this->fullname = implode( array($result->lastName, $result->firstName, $result->middleName), " " );
+		}
+
+		if ( isset( $result->trusted ) ) {
+			$this->trusted = ($result->trusted) ? 1 : 0;
+		}
+
+		if ( isset( $result->elements ) ) {
+			$this->requestUserDocs($result->elements, $token);
+		}
+	}
+
+	private function getRequestContext($token) {
+		return stream_context_create(array(
+			'http' => array(
+				'max_redirects' => 1,
+				'ignore_errors' => 1, // WTF???
+				'header'        => 'Authorization: Bearer '.$token,
+				'method'        => 'GET'
+			)
+		));
+	}
+
+	private function getUserDocCollection($url, $token) {
+		$result  = json_decode(file_get_contents($url, false, $this->getRequestContext($token)));
+		//print nl2br(str_replace(" ", "&nbsp;", print_r($result, true)));
+		if ($result->type === "PRG") {
+			$this->reg_region = (isset($result->region))   ? $result->region   : 0 ;
+			$this->reg_city   = (isset($result->city))     ? $result->city     : 0 ;
+			$this->reg_street = (isset($result->street))   ? $result->street   : 0 ;
+			$this->reg_house  = (isset($result->house))    ? $result->house    : 0 ;
+			$this->reg_frame  = (isset($result->frame))    ? $result->frame    : 0 ;
+			$this->reg_flat   = (isset($result->flat))     ? $result->flat     : 0 ;
+			$this->reg_fias   = (isset($result->fiasCode)) ? $result->fiasCode : 0 ;
+		}
+		if ($result->type === "PLV") {
+			$this->plv_region = (isset($result->region))   ? $result->region   : 0 ;
+			$this->plv_city   = (isset($result->city))     ? $result->city     : 0 ;
+			$this->plv_street = (isset($result->street))   ? $result->street   : 0 ;
+			$this->plv_house  = (isset($result->house))    ? $result->house    : 0 ;
+			$this->plv_frame  = (isset($result->frame))    ? $result->frame    : 0 ;
+			$this->plv_flat   = (isset($result->flat))     ? $result->flat     : 0 ;
+			$this->plv_fias   = (isset($result->fiasCode)) ? $result->fiasCode : 0 ;
+		}
+		if ($result->type === "EML") {
+			$this->email   = (isset($result->value))       ? $result->value." ".$result->vrfStu     : 0 ;
+		}
+		if ($result->type === "MBT") {
+			$this->cel_ph  = (isset($result->value))       ? $result->value." ".$result->vrfStu     : 0 ;
+		}
+	}
+
+	private function requestUserDocs($docList, $token) {
+		if ( !strlen($token) ) {
+			$this->logmodel->addToLog("Access token is missing. Aborting\n");
+			return false;
+		}
+		if ( !strlen($this->oid) ) {
+			$this->logmodel->addToLog("Object ID is missing. Aborting\n");
+			return false;
+		}
+		$this->logmodel->addToLog("\n------------------#-#-#------------------\nRequesting User Docs\n");
+		$output = array();
+		foreach ($docList as $url) {
+			$this->getUserDocCollection($url, $token);
+		}
+		//print nl2br(str_replace(" ", "&nbsp;", print_r($output, true)));
+		$this->logmodel->addToLog(print_r($output, true));
+		return $output;
+	}
+
+	private function checkRegion($userdata, $pattern) {
+		if ( $pattern->region === $userdata['prg']["region"] || $userdata['plv']["region"] ) {
+			return true;
+		}
+		return false;
+	}
+
+	private function checkCity($userdata, $pattern) {
+
+		//print_r($pattern->city);
+		$valid = false;
+		foreach ( $pattern->city as $city => $streets ) {
+			//print str_replace(".", "", $userdata['birthplace'])." - - ".$city;
+			if ( $city !== $userdata['prg']['city'] &&
+				 $city !== $userdata['plv']['city'] &&
+				 !stristr(str_replace(".", "", $userdata['birthplace']), $city)
+			) {
+				$valid = ($valid) ? true : false;
+			} else {
+				$valid = true;
+			}
+		}
+		return $valid;
+	}
+
+	private function checkStreet($userdata, $pattern) {
+		foreach ( $pattern->city as $city => $streets ) {
+			// если список улиц пустой, то подходит любая улица / город целиком
+			if ( !sizeof($streets) ) {
+				return 1;
+			}
+			foreach ($streets as $street => $houses) {
+				if ( sizeof($streets) && $street === $userdata['prg']["street"] || $street === $userdata['plv']["street"] ) {
+					$valid = 1;
+					// если список домов пустой, то подходит любая дом / улица целиком
+					if ( !sizeof($houses) ){
+						return 1;
+					}
+					// если дом входит в список домов на улице
+					if ( is_array($houses) && sizeof($houses) && ( in_array( $userdata['prg']["house"], $houses) || in_array($userdata['plv']["house"], $houses ) )) {
+						return 1;
+					}
+					return 0;
+				}
+			}
+		}
+	}
+
+	public function processUserMatching($userdata, $objectID) {
+		$pattern = json_decode(file_get_contents($this->config->item("base_server_path")."tickets/".$objectID));
+		//print_r($userdata);
+		//print nl2br(str_replace(" ", "&nbsp;", print_r($pattern, true)));
+		$pattern = $pattern->matchParams;
+		$valid = true;
+		if ( !isset($pattern->region) ) {
+			return 1;
+		}
+		if ( isset($pattern->region) ) {
+			$valid = $this->checkRegion($userdata, $pattern);
+		}
+		if ( $valid && isset($pattern->city) ) {
+			$valid = $this->checkCity($userdata, $pattern);
+		}
+		if ( $valid && isset($pattern->city) && is_object($pattern->city) ) {
+			$valid = $this->checkStreet($userdata, $pattern);
+		}
+		return $valid;
+	}
+}
---
+++ application/models/Verifymodel.php
@@ -0,0 +1,148 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Verifymodel extends CI_Model {
+	function __construct() {
+		parent::__construct();
+	}
+
+	
+	/* VERIFICATION */
+
+	/**
+	* Verifies an access token
+	* 
+	* @param $accessToken array
+	* @return true|false
+	*/
+	public function verifyToken($accessToken) {
+		// проверка токена ( Методические рекомендации по использованию ЕСИА v 2.23, Приложение В.6.4)
+		$this->logmodel->addToLog("\nTOKEN VERIFICATION\n");
+		if ( !$this->verifySignature($accessToken) ) {// ................. check signature !!
+			return false;
+		}
+		if ( !$this->verifyMnemonics($accessToken['payload']) ) {
+			return false;
+		};
+		if ( !$this->verifyExpiration($accessToken['payload']) ) {
+			return false;
+		};
+		if ( !$this->verifyIssuer($accessToken['payload']) ) {
+			return false;
+		};
+		return true;
+	}
+
+	/**
+	* Verifies a token issuer
+	* 
+	* @param $accessToken array
+	* @return true|false
+	*/
+	private function verifyIssuer($accessToken) {
+		if ($accessToken->iss === "http://esia.gosuslugi.ru/") {
+			$this->logmodel->addToLog("TOKEN ISSUER: ".$accessToken->iss." CORRECT!\n");
+			return true;
+		}
+		$this->logmodel->addToLog("\nTOKEN ISSUER FORGED!\n");
+		return false;
+	}
+
+	/**
+	* Verifies a mnemonics sent by ESIA to be a system of ours
+	* 
+	* @param $accessToken array
+	* @return true|false
+	*/
+	private function verifyMnemonics($accessToken) {
+		if ( !isset($accessToken->client_id) && $accessToken->client_id !== $this->config->item("IS_MNEMONICS") ) {
+			$this->logmodel->addToLog("MNEMONICS: ".$accessToken->client_id." - DO NOT MATCH!\n");
+			return false;
+		}
+		$this->logmodel->addToLog("MNEMONICS: CORRECT\n");
+		return true;
+	}
+
+	/**
+	* Verifies a token sent by ESIA whether it is applicable
+	* 
+	* @param $accessToken array
+	* @return true|false
+	*/
+	private function verifyExpiration($accessToken) {
+		$timeTolerance = 10; // 1 sec can cause failure.
+		if ( (int) date("U") < (int) ($accessToken->nbf - $timeTolerance) || (int) date("U") > (int) $accessToken->exp ) {
+			$this->logmodel->addToLog("ACTUAL: NO!\nNBF: ".$accessToken->nbf - $timeTolerance."( -".$timeTolerance." sec.),\nNOW: ".date("U").",\nEXP: ".$accessToken->exp."\n");
+			return false;
+		}
+		$this->logmodel->addToLog("ACTUAL: YES, BIAS: ".(date("U") - (int) $accessToken->nbf)." sec. (-".$timeTolerance." sec. tolerance)\n");
+		return true;
+	}
+
+	/**
+	* Verifies a signature sent by ESIA
+	* disabled
+	* 
+	* @param $accessToken array
+	* @return true|false
+	*/
+	private function verifySignature($accessToken) { // correct it later
+		$algs = array(
+			'RS256' => 'sha256'
+		);
+		$path        = $this->config->item("base_server_path").'application/views/esia/';
+		file_put_contents($path.'signature', utf8_encode($accessToken['signature']));
+		file_put_contents($path.'hashpart', utf8_encode($accessToken['hashpart']));
+		$result = exec("openssl dgst -sha256 -verify ".$path."cert/esia2.pem -signature ".$path."signature ".$path."hashpart");
+		$this->logmodel->addToLog( "SIGNATURE CHECK COMMAND:\n  openssl dgst -sha256 -verify ".$path."cert/esia2.pem -signature ".$path."signature ".$path."hashpart\nRESULT: ".$result."\n------------------\n" );
+		return true;
+		$this->logmodel->addToLog( "SIGNATURE CHECK RESULT: ".$result."\n------------------\n" );
+
+		$hash = $this->getSecret("HEADER.PAYLOAD");
+		if ( $hash === $accessToken['signature'] ) {
+			$this->logmodel->addToLog("SIGNATURE: MATCHED OK!\n");
+			return true;
+		}
+		$this->logmodel->addToLog( "COMPUTED SIGNATURE HASH:\n".$hash."\nDOES NOT MATCH ENCLOSED TOKEN HASH: \n".$accessToken['signature']."\n------------------\n" );
+		return false;
+	}
+
+	/**
+	* Verifies state previously given in return URL with the one provided by us
+	* 
+	* @param $state string
+	* @return string|false
+	*/
+	public function verifyState($state="") {
+		// проверка возвращённого кода состояния ( Методические рекомендации по использованию ЕСИА v 2.23, Приложение В.2.2)
+		if ( !strlen($state) ) {
+			$this->logmodel->addToLog("\nSTATE PARAMETER '".$this->input->get('state')."' WAS NOT SUPPLIED! Aborting!\n------------------");
+			$this->logmodel->writeLog();
+			return false;
+		}
+		if ( $this->input->get('state') === $state ) {
+			return true;
+		}
+		$this->logmodel->addToLog("\nSERVER RETURNED STATE PARAMETER '".$this->input->get('state')."' WHICH DOES NOT MATCH THE ONE SUPPLIED! Aborting!\n------------------");
+		$this->logmodel->writeLog();
+		return false;
+	}
+
+	public function verifyx() {
+		/*
+			openssl x509 -pubkey -noout -in wifi.sha256.crt > wifi.sha256.pem
+			openssl dgst -sha256 -verify /var/www/html/wifi/application/views/esia/cert/self/wifi.sha256.pem -signature signature hashpart
+		*/
+
+		$path   = $this->config->item("base_server_path").'application/views/esia/';
+		$result = null;
+		//$hashpart    = file_get_contents($path.'hashpart');
+		//$signature   = file_get_contents($path.'signature');
+		//$certContent = file_get_contents($path.'cert/self/wifi.sha256.crt');
+		//$cert        = openssl_pkey_get_public ( $certContent );
+		//json_decode($this->base64UrlSafeDecode($chunks[1])),
+		//$result      = openssl_verify ( $hashpart, $signature, $cert, "SHA256" );
+		//exec("openssl dgst -sha256 -verify ".$path."cert/self/wifi.sha256.crt -signature ".$path."signature ".$path."hashpart", $result);
+		var_dump($result);
+	}
+}
